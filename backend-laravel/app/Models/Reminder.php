a84bd2b5814a635a527726ba7c1d59af9c304fe6
eqinoox <neopavlovic.lazar@gmail.com>
2025-02-21 20:19:11 +0100

    Adding Models and their initial Migrations.

---
+++ backend-laravel/app/Models/Reminder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reminder extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['task_id', 'reminder_time', 'status'];
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
+}
